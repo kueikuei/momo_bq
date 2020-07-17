00f0f5e0bea5d68d68c40d8580a8efa642db6f5a
kueikuei <kueikuei@users.noreply.github.com>
2020-07-17 17:06:45 +0800

    Auto-commit: Fri Jul 17 2020 17:06:39 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
@@ -1,12 +1,5 @@
--- SELECT RANK ,
---     CASE RANK
---         WHEN (RANK > MAX(RANK)) THEN 't'
---     END
---     AS RES
--- FROM `momo-develop._script2c0037fd6eb32cd04a50af588f91ebcd31d5e3de.Example` 
--- -- ORDER BY rank
-
-SELECT 90 as A, 2 as B UNION ALL
-  SELECT 50, 8 UNION ALL
-  SELECT 60, 6 UNION ALL
-  SELECT 50, 10
+SELECT t.*, 
+case when rank<(select count() from momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2  ) 0.2 then 1 
+when rank>(select count(*) from momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2  ) * 0.8 then -1 
+else 0 end as t 
+FROM momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2 as t
