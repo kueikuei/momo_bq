b1bdc4255179b891285dbc9f3f7ee5905d7e4efd
kueikuei <kueikuei@users.noreply.github.com>
2020-07-17 17:45:01 +0800

    Auto-commit: Fri Jul 17 2020 17:44:45 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
@@ -1,7 +1,7 @@
 WITH t as(
 SELECT 
 RANK() OVER (ORDER BY order_3m_Sum DESC) AS Order_3m_Row_Sum_Rank,
-RANK() OVER (ORDER BY Order_1y_Sum DESC) AS Order_1y_Row_SumRank,
+-- RANK() OVER (ORDER BY Order_1y_Sum DESC) AS Order_1y_Row_SumRank,
 *
 FROM `momo-develop._scriptb0c309b28d8dab4961b6dd2b0a8a9de50e51b162.Example2`
 )
@@ -10,9 +10,9 @@ FROM `momo-develop._scriptb0c309b28d8dab4961b6dd2b0a8a9de50e51b162.Example2`
     case when Order_3m_Row_Sum_Rank<(select count(*) from t  ) * 0.2 then 1 
     when Order_3m_Row_Sum_Rank>(select count(*) from t  ) * 0.8 then -1 
     else 0 end as Order_3m_Row_Sum_Rank_ResRate,
-    case when Order_1y_Row_SumRank<(select count(*) from t  ) * 0.2 then 1 
-    when Order_1y_Row_SumRank>(select count(*) from t  ) * 0.8 then -1 
-    else 0 end as Order_1y_Row_SumRank_ResRate,
+    -- case when Order_1y_Row_SumRank<(select count(*) from t  ) * 0.2 then 1 
+    -- when Order_1y_Row_SumRank>(select count(*) from t  ) * 0.8 then -1 
+    -- else 0 end as Order_1y_Row_SumRank_ResRate,
     t.* 
     from t
 )
