df29f984a19a1c9a93bd3279ef99d2e024f0afdb
kueikuei <kueikuei@users.noreply.github.com>
2020-07-17 17:01:17 +0800

    Auto-commit: Fri Jul 17 2020 17:01:05 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
@@ -1,1 +1,12 @@
-SELECT * FROM `momo-develop._script2c0037fd6eb32cd04a50af588f91ebcd31d5e3de.Example` LIMIT 100
+-- SELECT RANK ,
+--     CASE RANK
+--         WHEN (RANK > MAX(RANK)) THEN 't'
+--     END
+--     AS RES
+-- FROM `momo-develop._script2c0037fd6eb32cd04a50af588f91ebcd31d5e3de.Example` 
+-- -- ORDER BY rank
+
+SELECT 90 as A, 2 as B UNION ALL
+  SELECT 50, 8 UNION ALL
+  SELECT 60, 6 UNION ALL
+  SELECT 50, 10
