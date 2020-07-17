e026f280123440f9f728449aac9057668ae33979
kueikuei <kueikuei@users.noreply.github.com>
2020-07-17 17:10:46 +0800

    Auto-commit: Fri Jul 17 2020 17:10:40 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
@@ -1,5 +1,8 @@
-SELECT t.*, 
-case when rank<(select count(*) from momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2  ) * 0.2 then 1 
-when rank>(select count(*) from momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2  ) * 0.8 then -1 
-else 0 end as t 
-FROM momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2 as t
+-- SELECT t.*, 
+-- case when rank<(select count(*) from momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2  ) * 0.2 then 1 
+-- when rank>(select count(*) from momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2  ) * 0.8 then -1 
+-- else 0 end as t 
+-- FROM momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2 as t
+
+
+select counmt(*) from momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2
