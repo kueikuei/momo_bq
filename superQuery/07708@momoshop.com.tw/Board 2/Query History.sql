17812af5ee3e8224a5b520189ae884352d116511
kueikuei <kueikuei@users.noreply.github.com>
2020-07-20 18:15:50 +0800

    Auto-commit: Mon Jul 20 2020 18:15:43 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
@@ -13,7 +13,7 @@ SELECT
         -- 近一年為熱銷品且不為新品 -> 穩定熱銷品 || 長期活動贈品 (ex 理膚寶水)
         WHEN (New_Goods_Or_Not = false AND Order_1y_Row_Sum_Rank_Rate = true AND Order_1y_Never_BeSold_Good = false) THEN '3'
         -- 近三個月為長尾商品(Sum>0)且為新品 -> 可能賣的時間不夠長
-        WHEN (New_Goods_Or_Not = true AND Order_3m_Row_Sum_Rank_Rate = false AND Order_1y_Never_BeSold_Good = false AND Order_3m_Never_BeSold_Good = false) THEN '4'
+        WHEN (New_Goods_Or_Not = true AND Order_3m_Row_Sum_Rank_Rate = false AND Order_3m_Never_BeSold_Good = false) THEN '4'
         -- 近一年為長尾商品(扣掉三個月的量Sum>0)且不為新品 -> 真的很不暢銷的商品
         WHEN (New_Goods_Or_Not = false AND Order_3m_Row_Sum_Rank_Rate = false AND Order_1y_Row_Sum_Rank_Rate = false AND Order_1y_Never_BeSold_Good = false AND Order_3m_Never_BeSold_Good = false) THEN '5'
         -- 近一年銷量為0且不為新品 -> 超級冷門貨
