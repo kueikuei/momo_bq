8170dc4742e2706081ce35b6a585c2964bc29b05
kueikuei <kueikuei@users.noreply.github.com>
2020-07-20 15:36:56 +0800

    Auto-commit: Mon Jul 20 2020 15:36:50 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Tab 9.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Tab 9.sql
@@ -1,3 +1,4 @@
 select * from `momo-develop._3b7a5bd540a8dfd341e57f295848b5793cf3d3ab.anone14e1434_abf1_4555_b466_e6d1c0176ef8`
-where Order_1y_Row_Sum_Rank_Rate = true AND New_Goods_Or_Not = true
+where New_Goods_Or_Not = true
+-- where Order_1y_Row_Sum_Rank_Rate = true AND New_Goods_Or_Not = true
 -- limit 100
