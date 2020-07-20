db5e0df17621343dbafa882fdcccd9bee6ec3330
kueikuei <kueikuei@users.noreply.github.com>
2020-07-20 11:15:30 +0800

    Auto-commit: Mon Jul 20 2020 11:15:24 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
@@ -1,5 +1,16 @@
 SELECT 
-    IF(date(INSERT_DATE)  >= date_sub(current_date, interval 1 month),true,false) as One_Month_Or_Not,
+    case when Order_3m_Row_Sum_Rank<(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`   ) * 0.2 then 1
+    when Order_3m_Row_Sum_Rank>(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`   ) * 0.8 then -1
+    else 0 end as Order_3m_Row_Sum_Rank_Rate ,
+    case when Order_1y_Row_SumRank<(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`   ) * 0.2 then 1
+    when Order_1y_Row_SumRank>(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`   ) * 0.8 then -1
+    else 0 end as Order_1y_Row_SumRank_Rate ,
     * 
-FROM `momo-develop._scriptcfd0a84ff2415dba64c139518b670410e782e721.GrayGoods` 
+FROM `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods` 
 limit 1000
+
+-- SELECT t.*, 
+-- case when rank<(select count(*) from momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2  ) * 0.2 then 1 
+-- when rank>(select count(*) from momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2  ) * 0.8 then -1 
+-- else 0 end as t 
+-- FROM momo-develop._script790dc6b1551f5eb0fd39117aeb08d41eed4d9beb.Example2 as t
