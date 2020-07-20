6b9f24c81bb99c610310a3936f37405a744586a2
kueikuei <kueikuei@users.noreply.github.com>
2020-07-20 15:31:50 +0800

    Auto-commit: Mon Jul 20 2020 15:31:36 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Tab 11.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Tab 11.sql
@@ -1,1 +1,6 @@
-select * from `momo-develop.oggSync.TGOODSDT` limit 1000
+WITH T AS(
+    select CONCAT(GOODS_CODE,GOODSDT_CODE) AS TTT,* from `momo-develop.oggSync.TGOODSDT` as t
+-- where  = '7826949001'
+)
+SELECT * FROM T
+WHERE TTT = '7826949001'
