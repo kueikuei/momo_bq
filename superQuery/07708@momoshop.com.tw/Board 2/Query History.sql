0b4ad29c19d56eb7fef089b959149b8cc6584478
kueikuei <kueikuei@users.noreply.github.com>
2020-07-20 09:51:32 +0800

    Auto-commit: Mon Jul 20 2020 09:51:27 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
@@ -1,4 +1,5 @@
 SELECT 
-    IF(date(INSERT_DATE)  >= date_sub(current_date, interval 1 month),true,false),
+    IF(date(INSERT_DATE)  >= date_sub(current_date, interval 1 month),true,false) as One_Month_Or_Not,
     * 
-FROM `momo-develop._scriptcfd0a84ff2415dba64c139518b670410e782e721.GrayGoods`
+FROM `momo-develop._scriptcfd0a84ff2415dba64c139518b670410e782e721.GrayGoods` 
+limit 1000
