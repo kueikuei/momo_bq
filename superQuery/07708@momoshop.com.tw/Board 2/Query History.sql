1367030a8601f800317608d82e8a01cab1b9219a
kueikuei <kueikuei@users.noreply.github.com>
2020-07-20 17:02:00 +0800

    Auto-commit: Mon Jul 20 2020 17:01:46 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
@@ -1,1 +1,13 @@
-select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`
+WITH t as(
+    select * from `momo-develop._3b7a5bd540a8dfd341e57f295848b5793cf3d3ab.anone14e1434_abf1_4555_b466_e6d1c0176ef8`
+)
+
+SELECT
+    Full_Goodscode,
+    CASE
+        WHEN (New_Goods_Or_Not = true AND Order_3m_Row_Sum_Rank_Rate = true AND Order_1y_Row_Sum_Rank_Rate = true AND Order_1y_Never_BeSold_Good = true AND Order_3m_Never_BeSold_Good = true) THEN '1'
+        WHEN (New_Goods_Or_Not = true AND Order_3m_Row_Sum_Rank_Rate = true AND Order_1y_Row_Sum_Rank_Rate = true AND Order_1y_Never_BeSold_Good = true AND Order_3m_Never_BeSold_Good = false) THEN '2'
+        ELSE '0'
+        END
+    AS category
+from t
