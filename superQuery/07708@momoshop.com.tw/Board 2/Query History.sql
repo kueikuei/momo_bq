6978bd104ee7360fdf194a13a9a2a31deacaf38a
kueikuei <kueikuei@users.noreply.github.com>
2020-07-20 13:07:19 +0800

    Auto-commit: Mon Jul 20 2020 13:07:12 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Query History.sql
@@ -1,15 +1,10 @@
 SELECT 
-    case when Order_3m_Row_Sum_Rank<(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`   ) * 0.2 then 1
-    when Order_3m_Row_Sum_Rank>(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`   ) * 0.2 then 0
-    else 0 end as Order_3m_Row_Sum_Rank_Rate ,
-    -- case when Order_1y_Row_SumRank<(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`   ) * 0.2 then 1
-    -- when Order_1y_Row_SumRank>(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`   ) * 0.2 then 0
-    -- else 0 end as Order_1y_Row_SumRank_Rate ,
-    IF(Order_1y_Row_SumRank<(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`   ) * 0.2, 'true', 'false') as Order_1y_Row_SumRank_Rate,
+    IF(Order_3m_Row_Sum_Rank<(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`) * 0.2, 'true', 'false') as Order_3m_Row_Sum_Rank_Rate,
+    IF(Order_1y_Row_SumRank<(select count(*) from `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`) * 0.2, 'true', 'false') as Order_1y_Row_SumRank_Rate,
     IF(Order_1y_Sum=0, 'true', 'false') as Order_1y_Never_BeSold_Good,
     IF(Order_3m_Sum=0, 'true', 'false') as Order_3m_Never_BeSold_Good,
     * 
-FROM `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods` 
+FROM `momo-develop._scripte54f6fb8d4fa08f21a73a943578694cd92ee7544.GrayGoods`  as t
 limit 1000
 
 -- SELECT t.*, 
