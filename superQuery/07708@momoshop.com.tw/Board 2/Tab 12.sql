6f885ea187f2306fbf6ecb95bfbeb02c057e9571
kueikuei <kueikuei@users.noreply.github.com>
2020-07-21 13:45:06 +0800

    Auto-commit: Tue Jul 21 2020 13:45:01 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Tab 12.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Tab 12.sql
@@ -1,33 +1,3 @@
-WITH t1 as (
-    select orderNo,count(*) as t1ct from `momo-develop.boxSaver.regularQC_slipInfo_3m`
-    where 
-        brand_chi LIKE '%理膚寶水%' AND 
-        delyType = '乙配' 
-    GROUP BY orderNo
-),
-t2 AS(
-    select t2.orderNo,count(*) as ct, count(distinct t2.brand_chi) as isSameBrand_chi from `momo-develop.boxSaver.regularQC_slipInfo_3m` as t2
-    GROUP BY t2.orderNo
-)
-,
-t4 as (
-    select t1.* ,t2.ct, t2.isSameBrand_chi
-    from t1 as t1
-    left join t2 on t1.orderNo = t2.orderNo
-),
-t5 as (
-select *,(t1ct-ct) as num from t4
-where isSameBrand_chi = 1
-)
-
-select * from t5
-where num = 0
-
-
-
--- SELECT 
---  ttt.*,Table_R.Value
--- FROM Table_L as ttt
--- left join Table_R  on ttt.Row = Table_R.Row
-
--- where date(orderdate)=DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY) and delytype='乙配'
+SELECT COUNT(*)
+FROM `momo-develop._3b7a5bd540a8dfd341e57f295848b5793cf3d3ab.anon561eeca6_369c_4d51_9d2f_b23e7f437906`
+WHERE category = '1' AND Order_3m_Never_BeSold_Good != true AND Order_1y_Never_BeSold_Good != true
