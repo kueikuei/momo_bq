b51ac7793d8be9d2c34e0c46412556efdd1118ac
kueikuei <kueikuei@users.noreply.github.com>
2020-07-21 13:48:19 +0800

    Auto-commit: Tue Jul 21 2020 13:48:14 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 2/Tab 12.sql
+++ superQuery/07708@momoshop.com.tw/Board 2/Tab 12.sql
@@ -1,5 +1,5 @@
 SELECT category,COUNT(*)
 FROM `momo-develop._3b7a5bd540a8dfd341e57f295848b5793cf3d3ab.anon561eeca6_369c_4d51_9d2f_b23e7f437906`
 -- WHERE category = '1' 
+WHERE Order_3m_Never_BeSold_Good != true AND Order_1y_Never_BeSold_Good != true
 GROUP BY category
--- AND Order_3m_Never_BeSold_Good != true AND Order_1y_Never_BeSold_Good != true
