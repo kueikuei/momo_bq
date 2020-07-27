0960be800d1da7f6f1015368846c1b63b59f2d4e
kueikuei <kueikuei@users.noreply.github.com>
2020-07-27 11:56:28 +0800

    Auto-commit: Mon Jul 27 2020 11:56:22 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
@@ -1,3 +1,7 @@
+-- select * 
+-- from `boxSaver.slipFinishInfo` 
+-- where slipNo = '200001491698'
+
 select * 
-from `boxSaver.slipFinishInfo` 
+from `boxSaver.1129slipInfo` 
 where slipNo = '200001491698'
