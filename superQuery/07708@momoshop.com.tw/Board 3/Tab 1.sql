31f6d5c1089df7d90f2960f0b129dc5a5bba792c
kueikuei <kueikuei@users.noreply.github.com>
2020-07-27 17:44:08 +0800

    Auto-commit: Mon Jul 27 2020 17:44:00 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
@@ -1,7 +1,28 @@
--- select * 
--- from `boxSaver.slipFinishInfo` 
--- where slipNo = '200001491698'
+-- 取得每一個商品的長寬高、最小投影面積、最長邊
+with dim as
+(
+  select 
+    distinct t.GOODS_CODE,t.WIDTH ,t.LENGTH ,t.HEIGHT , 
+    -- 計算最小投影面積
+    LEAST(WIDTH*LENGTH, LENGTH*HEIGHT, WIDTH*HEIGHT) as minProjectArea,
+    -- 取得最長邊      
+    GREATEST(t.WIDTH ,t.LENGTH ,t.HEIGHT) as lagestSide
+  from `oggSync.FUBON_TGOODS`  as t  
+),
+-- delyQty 裝箱數量?
+-- 以每一個訂單作為一個單位
+-- 取得內容物的 goodscode 以及其 delyQty 裝箱數量
+-- 為何需要取得裝箱數量
+slipNoInfo as (
+  select t.slipNo , array_agg(struct(t.goodsCode, t.delyQty)) as info   
+  from `boxSaver.slipFinishInfo` as t
+  where t.orderDate = '2020-07-14'
+  group by t.slipNo 
+)
 
-select * 
-from `boxSaver.slipInfo` 
-where slipNo = '10000154268173'
+select * from slipNoInfo
+
+-- select t1.*
+-- from slipNoInfo as t1 
+-- left join dim as t2
+-- on t1.goodsCode = t2.GOODS_CODE
