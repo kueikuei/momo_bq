f564278b8440fb2c265f5e848ce025f3fe4acea6
kueikuei <kueikuei@users.noreply.github.com>
2020-07-28 10:28:56 +0800

    Auto-commit: Tue Jul 28 2020 10:28:41 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
@@ -1,3 +1,4 @@
+-- step1 先取2020 1~5月3 樓所有資料
 -- 取得每一個商品的長寬高、最小投影面積、最長邊
 with dim as
 (
@@ -9,20 +10,30 @@ with dim as
     GREATEST(t.WIDTH ,t.LENGTH ,t.HEIGHT) as lagestSide
   from `oggSync.FUBON_TGOODS`  as t  
 ),
+-- step2 定義特定時間(未來改成blue資料)去 merge 所有箱子長寬高、最小投影面積、最長邊
+getInfo as(
+    select t2.*, t.slipNo, t.delyQty, t.boxName 
+    from `boxSaver.slipFinishInfo` as t
+    left join dim as t2
+    on t.goodsCode = t2.GOODS_CODE
+)
+-- select count(*) from getInfo
+select * from getInfo
+ 
 -- delyQty 裝箱數量?
 -- 以每一個訂單作為一個單位
 -- 取得內容物的 goodscode 以及其 delyQty 裝箱數量
 -- 為何需要取得裝箱數量
-slipNoInfo as (
-  select t.slipNo , array_agg(struct(t.goodsCode, t.delyQty)) as info   
-  from `boxSaver.slipFinishInfo` as t
-  where t.orderDate = '2020-07-14'
-  group by t.slipNo 
-)
+-- slipNoInfo as (
+--   select t.slipNo , array_agg(struct(t.goodsCode, t.delyQty)) as info   
+--   from `boxSaver.slipFinishInfo` as t
+-- --   where t.orderDate = '2020-07-14'
+--   group by t.slipNo 
+-- )
 
-select UNNEST(info) from slipNoInfo
+-- select * from slipNoInfo
 
 -- select t1.*
--- from slipNoInfo as t1 
--- left join dim as t2
--- on t1.goodsCode = t2.GOODS_CODE
+-- from slipNoInfo.goodsCode as t1 
+-- cross join dim.GOODS_CODE
+-- t2.GOODS_CODE
