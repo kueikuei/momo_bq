09c98259bf50a23391287e4242ae0fc52089d8e7
kueikuei <kueikuei@users.noreply.github.com>
2020-07-30 16:54:45 +0800

    Auto-commit: Thu Jul 30 2020 16:54:37 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,3 +1,3 @@
-select t.goodsName , t.delyQty , t.orderNo, t.slipNo 
+select t.goodsCode , t.delyQty , t.orderNo, t.slipNo 
 from boxSaver.slipFinishInfo as t
 where t.slipNo = '95000002846493' OR t.slipNo = '95000002846494'
