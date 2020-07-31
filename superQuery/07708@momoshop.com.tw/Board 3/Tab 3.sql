f90c9aa0cb73f4ca414965dfbc8e60f46d48691f
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 14:36:16 +0800

    Auto-commit: Fri Jul 31 2020 14:36:02 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,25 +1,4 @@
-
--- 找出在Q台拆箱 box，並且取得新的 slipNo
-WITH ipack_up_1 as(
-    SELECT
-        slipNo,
-        count(slipNo) as num
+SELECT
+        count(slipNo)
     from `momo-develop.ipacking.ipack_data` 
     group by slipNo
-    having num > 1
-)
-,
-ipack_equal_1 as(
-    SELECT
-        slipNo
-    from `momo-develop.ipacking.ipack_data` 
-    group by slipNo
-)
-, newSlipNO as(
-    select t2.newSlipNo as slipNo
-    from ipack_up_1 as t1
-    left join `boxSaver.addBoxInfo` as t2
-    on t1.slipNo = t2.orgSlipNo
-    WHERE t2.newSlipNo is not null
-)
-select count(*) from newSlipNO
