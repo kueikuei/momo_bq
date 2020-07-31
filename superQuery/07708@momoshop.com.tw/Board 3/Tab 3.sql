7bdaf8e70f423ded18e2e0199f42de4346644354
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 15:09:50 +0800

    Auto-commit: Fri Jul 31 2020 15:09:33 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,6 +1,3 @@
-SELECT
-        slipNo,
-        count(slipNo) as num
-    from `momo-develop.ipacking.ipack_data` 
-    group by slipNo
-    having num = 1
+select t2.orderNo,t1.* from `momo-develop._3b7a5bd540a8dfd341e57f295848b5793cf3d3ab.anon4ed9dcdea16f2430f50c78588a8abcff03db6267` as t1
+left join `boxSaver.slipFinishInfo` as t2
+on t1.slipNo = t2.slipNo
