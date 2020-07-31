5c49be7c14a2677f21633506a68caeb59a091d18
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 17:58:50 +0800

    Auto-commit: Fri Jul 31 2020 17:58:42 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
@@ -1,5 +1,5 @@
-select b.boxName
+select b.boxName, count(b.boxName)
 -- select *, b.*
 from momo-develop.ipacking.ipack_temp_new as t, unnest(t.info) as b
 group by b.boxName
--- where slipNo like '95%'
+order by b.boxName desc
