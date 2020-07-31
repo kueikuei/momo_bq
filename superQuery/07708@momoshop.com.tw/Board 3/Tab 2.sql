11e396c2b53d4809626ea974e24478f6e57cf8a2
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 17:41:28 +0800

    Auto-commit: Fri Jul 31 2020 17:41:11 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
@@ -1,16 +1,1 @@
--- CREATE TEMP TABLE HELLO_IPAC AS
--- select * from `momo-develop._3b7a5bd540a8dfd341e57f295848b5793cf3d3ab.anon05c0a3b26f28da7166f58c5060ef378e2df4d256`;
--- select * from HELLO_IPAC
--- -- where t.info.HEIGHT is NULL OR t.info.HEIGHT is NULL OR t.info.WIDTH is NULL 
-
-
-CREATE TABLE `momo-develop.ipacking.ipack_temp_new`
-OPTIONS(
-expiration_timestamp=TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL 14 DAY)
-) AS
-SELECT *
-FROM `momo-develop._3b7a5bd540a8dfd341e57f295848b5793cf3d3ab.anon35fcf2040779da70cfc117049998de161773c8d0`
-
--- select * from `momo-develop.ipacking.ipack_temp`
-
--- select * from HELLO_IPAC
+select * from `momo-develop.ipacking.ipack_temp_new`
