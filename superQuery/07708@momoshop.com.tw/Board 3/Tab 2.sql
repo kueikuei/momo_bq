6fbe0a9a5399c06c5744ec7e01fc0af3820d492a
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 18:23:22 +0800

    Auto-commit: Fri Jul 31 2020 18:23:15 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
@@ -1,3 +1,12 @@
-select *
-from momo-develop.ipacking.ipack_temp_new
-where slipNo = '10000263569561'
+WITH t AS(
+    select t.slipNo, b.boxName, count(b.boxName) as num
+    from momo-develop.ipacking.ipack_temp_new as t, unnest(t.info) as b
+    where b.boxName is not null
+    group by t.slipNo,b.boxName
+    order by num desc
+)
+
+select boxName , count(b.boxName) as num , CONCAT(count(b.boxName)/1852957*100,'%') as rate
+from t
+group by boxName
+order by num desc
