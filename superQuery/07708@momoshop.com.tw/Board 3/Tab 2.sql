5c7bd91f5fd31208de407a662a4fb0e530b866dd
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 18:12:32 +0800

    Auto-commit: Fri Jul 31 2020 18:12:24 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
@@ -1,5 +1,7 @@
-select b.boxName, count(b.boxName) as num
+
+select b.boxName, count(b.boxName) as num, CONCAT(count(b.boxName)/8035462*100,'%')
 -- select *, b.*
 from momo-develop.ipacking.ipack_temp_new as t, unnest(t.info) as b
 group by b.boxName
 order by num desc
+-- where slipNo li
