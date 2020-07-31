746b56d036a38cdfbb561ed7a47b9802bbb7f653
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 17:13:48 +0800

    Auto-commit: Fri Jul 31 2020 17:13:37 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
@@ -1,2 +1,3 @@
-select count(UNNEST(*)) from `momo-develop._3b7a5bd540a8dfd341e57f295848b5793cf3d3ab.anon05c0a3b26f28da7166f58c5060ef378e2df4d256` as t
-where t.info.HEIGHT is NULL OR t.info.HEIGHT is NULL OR t.info.WIDTH is NULL
+CREATE TEMP TABLE HELLO_IPAC AS
+select * from `momo-develop._3b7a5bd540a8dfd341e57f295848b5793cf3d3ab.anon05c0a3b26f28da7166f58c5060ef378e2df4d256` as t;
+-- where t.info.HEIGHT is NULL OR t.info.HEIGHT is NULL OR t.info.WIDTH is NULL 
