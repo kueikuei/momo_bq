7adae135c0f882be33e6ffde45881b9c326fd6a1
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 16:08:44 +0800

    Auto-commit: Fri Jul 31 2020 16:08:38 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,12 +1,3 @@
-with dim as
-(
-  select 
-    distinct t.GOODS_CODE,t.WIDTH ,t.LENGTH ,t.HEIGHT , 
-    -- 計算最小投影面積
-    LEAST(WIDTH*LENGTH, LENGTH*HEIGHT, WIDTH*HEIGHT) as minProjectArea,
-    -- 取得最長邊      
-    GREATEST(t.WIDTH ,t.LENGTH ,t.HEIGHT) as lagestSide
-  from `oggSync.FUBON_TGOODS`  as t  
-)
-
-select * from dim
+select count(*) 
+from `oggSync.FUBON_TGOODS`
+where WIDTH = NULL OR HEIGHT = NULL OR LENGTH = NULL
