70547358eaa1117b589ddc30bff74e4a2226dacc
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 17:46:41 +0800

    Auto-commit: Fri Jul 31 2020 17:46:33 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
@@ -1,2 +1,3 @@
-select t.slipNo, b.*
-from momo-develop.ipacking.ipack_temp as t, unnest(t.info) as b
+select count(*)
+from momo-develop.ipacking.ipack_temp_new as t, unnest(t.info) as b
+where b.HEIGHT is NULL OR b.HEIGHT is NULL OR b.WIDTH is NULL
