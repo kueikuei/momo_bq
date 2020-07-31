c3dea75908523b9b115926886b1780bd4adacac0
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 13:44:48 +0800

    Auto-commit: Fri Jul 31 2020 13:44:36 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,6 +1,9 @@
-  SELECT
+WITH Q_unboxing as(
+    SELECT
         slipNo,
         count(slipNo) as num
     from `momo-develop.ipacking.ipack_data` 
     group by slipNo
-    having num = 1
+     having num > 1
+)
+select sum(num) from Q_unboxing
