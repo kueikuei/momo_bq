335f7bc06370ab81f6f15ed40b225efce061735f
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 18:16:16 +0800

    Auto-commit: Fri Jul 31 2020 18:16:01 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
@@ -1,9 +1,3 @@
-
-select b.boxName, count(b.boxName) as num, CONCAT(count(b.boxName)/8035462*100,'%') as rate
--- select *, b.*
+select t.slipNo, b.boxName
 from momo-develop.ipacking.ipack_temp_new as t, unnest(t.info) as b
-group by b.boxName
-order by num desc
--- where slipNo like '95%'
--- where b.boxName is NULL
--- where b.HEIGHT is NULL
+group by t.slipNo,b.boxName
