e956f31861958639aa8ab58dfd6c777425a34807
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 16:54:59 +0800

    Auto-commit: Fri Jul 31 2020 16:54:42 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
@@ -1,8 +1,21 @@
-select t.slipNo 
-from boxSaver.slipInfo as t
-where t.slipNo in (
-	select t.newSlipNo 
-	from boxSaver.addBoxInfo as t
-	where t.orgSlipNo ='10000255227448'
+-- step1 先取2020 1~5月3 樓所有資料
+-- 取得每一個商品的長寬高、最小投影面積、最長邊
+with dim as
+(
+  select 
+    distinct t.GOODS_CODE,t.WIDTH ,t.LENGTH ,t.HEIGHT , 
+    -- 計算最小投影面積
+    LEAST(WIDTH*LENGTH, LENGTH*HEIGHT, WIDTH*HEIGHT) as minProjectArea,
+    -- 取得最長邊      
+    GREATEST(t.WIDTH ,t.LENGTH ,t.HEIGHT) as lagestSide
+  from `oggSync.TGOODS` as t  
 )
-group by t.slipNo
+-- step2 slipFinishInfo 先與 TGOODS 合併
+,slipFinishInfoMergeTGOODS as(
+    select t1.*, t2.*
+    from `boxSaver.slipFinishInfo` as t1
+    left join dim as t2 
+    on t1.goodsCode = t2.GOODS_CODE
+) 
+
+select * from slipFinishInfoMergeTGOODS
