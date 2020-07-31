7e7f624ded2bb3d1201cbfb662d058ff4d96efbd
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 13:15:33 +0800

    Auto-commit: Fri Jul 31 2020 13:15:25 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 2.sql
@@ -1,7 +1,8 @@
-select t.slipNo, t.goodsName , t.delyQty , t.orderNo 
+select t.slipNo 
 from boxSaver.slipInfo as t
 where t.slipNo in (
 	select t.newSlipNo 
 	from boxSaver.addBoxInfo as t
 	where t.orgSlipNo ='10000255227448'
 )
+group by t.slipNo
