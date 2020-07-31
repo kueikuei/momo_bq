d11099a5c3c6884feadee2b76b78d5c5a6304232
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 14:02:56 +0800

    Auto-commit: Fri Jul 31 2020 14:02:47 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,2 +1,3 @@
-select * from `momo-develop._3b7a5bd540a8dfd341e57f295848b5793cf3d3ab.anonf997998579ad2b1049709c6dab8f990f93ef52e5`
+
+select * from `momo-develop.boxSaver.slipInfo`
 where slipNo = '10000265198098'
