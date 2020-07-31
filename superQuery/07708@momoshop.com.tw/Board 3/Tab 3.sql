0a173fe898ffa446f972e476d7edcafabf16023e
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 14:15:02 +0800

    Auto-commit: Fri Jul 31 2020 14:14:55 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,5 +1,13 @@
-SELECT
-    slipN,
-    slipNo
-from `momo-develop.ipacking.ipack_data` 
-where slipNo = '10000265198098'
+WITH Q_unboxing as(
+    SELECT
+        slipNo,
+        count(slipNo) as num
+    from `momo-develop.ipacking.ipack_data` 
+    group by slipNo
+     having num > 1
+)
+select t1.slipNo, t2.newSlipNo
+from Q_unboxing as t1
+left join `boxSaver.addBoxInfo` as t2
+on t1.slipNo = t2.orgSlipNo
+HAVING t2.newSlipNo is not null
