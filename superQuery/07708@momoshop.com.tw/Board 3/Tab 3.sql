bed18d8822fed7252c7475514246d9ee11b8c38e
kueikuei <kueikuei@users.noreply.github.com>
2020-07-31 14:16:55 +0800

    Auto-commit: Fri Jul 31 2020 14:16:39 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -10,4 +10,4 @@ select t1.slipNo, t2.newSlipNo
 from Q_unboxing as t1
 left join `boxSaver.addBoxInfo` as t2
 on t1.slipNo = t2.orgSlipNo
-HAVING t2.newSlipNo is not null
+WHERE t2.newSlipNo is not null
