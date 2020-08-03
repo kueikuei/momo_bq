da7e9e71fc4c6a1dbdce3df5bf05f23facb96b37
kueikuei <kueikuei@users.noreply.github.com>
2020-08-03 11:42:40 +0800

    Auto-commit: Mon Aug 03 2020 11:42:32 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
@@ -1,4 +1,5 @@
-SELECT "800M" AS race,
+WITH races AS (
+  SELECT "800M" AS race,
     [STRUCT("Rudisha" as name, [23.4, 26.3, 26.4, 26.1] as splits),
      STRUCT("Makhloufi" as name, [24.5, 25.4, 26.6, 26.1] as splits),
      STRUCT("Murphy" as name, [23.9, 26.0, 27.0, 26.0] as splits),
@@ -7,4 +8,8 @@ SELECT "800M" AS race,
      STRUCT("Lewandowski" as name, [25.0, 25.7, 26.3, 27.2] as splits),
      STRUCT("Kipketer" as name, [23.2, 26.1, 27.3, 29.4] as splits),
      STRUCT("Berian" as name, [23.7, 26.1, 27.0, 29.3] as splits)]
-       AS participants
+       AS participants)
+       
+SELECT SUM(duration)
+FROM UNNEST(splits) AS duration) ASC
+LIMIT 1
