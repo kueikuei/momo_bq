3133b1e12313dd14bb5a7896e671e72d829d7cac
kueikuei <kueikuei@users.noreply.github.com>
2020-08-03 11:17:16 +0800

    Auto-commit: Mon Aug 03 2020 11:17:09 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
@@ -1,3 +1,8 @@
+WITH t as(
 SELECT 1 AS id, [0, 1, 1, 2, 3, 5] AS some_numbers
    UNION ALL SELECT 2 AS id, [2, 4, 8, 16, 32] AS some_numbers
    UNION ALL SELECT 3 AS id, [5, 10] AS some_numbers
+)
+
+select t.id,b.*
+from t,unnest(t.some_numbers) as b
