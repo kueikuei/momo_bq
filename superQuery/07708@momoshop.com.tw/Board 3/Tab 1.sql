e34723a32dc2aebf5ba767dfa775bfa69909f8bd
kueikuei <kueikuei@users.noreply.github.com>
2020-08-04 14:35:20 +0800

    Auto-commit: Tue Aug 04 2020 14:35:09 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
@@ -1,12 +1,6 @@
-SELECT "800M" AS race,
-[STRUCT("Rudisha" as name, [23.4, 26.3, 26.4, 26.1] as splits),
-STRUCT("Makhloufi" as name, [24.5, 25.4, 26.6, 26.1] as splits),
-STRUCT("Murphy" as name, [23.9, 26.0, 27.0, 26.0] as splits),
-STRUCT("Bosse" as name, [23.6, 26.2, 26.5, 27.1] as splits),
-STRUCT("Rotich" as name, [24.7, 25.6, 26.9, 26.4] as splits),
-STRUCT("Lewandowski" as name, [25.0, 25.7, 26.3, 27.2] as splits),
-STRUCT("Kipketer" as name, [23.2, 26.1, 27.3, 29.4] as splits),
-STRUCT("Berian" as name, [23.7, 26.1, 27.0, 29.3] as splits),
-STRUCT("Nathan" as name, ARRAY<FLOAT64>[] as splits),
-STRUCT("David" as name, NULL as splits)]
-AS participants
+SELECT 
+CONCAT('https://stackoverflow.com/questions/', CAST(id as STRING)) as url, 
+view_count 
+FROM `bigquery-public-data.stackoverflow.posts_questions` 
+WHERE tags like '%google-bigquery%' 
+ORDER BY favorite_count DESC LIMIT 10
