19bb22dd80030ebac2008d2b6321f34bcb86a75a
kueikuei <kueikuei@users.noreply.github.com>
2020-08-04 14:39:45 +0800

    Auto-commit: Tue Aug 04 2020 14:39:36 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
@@ -1,6 +1,5 @@
 SELECT 
-CONCAT('https://stackoverflow.com/questions/', CAST(id as STRING)) as url, 
-view_count 
+CONCAT('https://stackoverflow.com/questions/', CAST(id as STRING)) as url, view_count 
 FROM `bigquery-public-data.stackoverflow.posts_questions` 
 WHERE tags like '%google-bigquery%' 
 ORDER BY favorite_count DESC LIMIT 10
