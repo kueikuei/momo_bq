667bb0491294ec1bdf02f0b4a30aa9505edf25ad
kueikuei <kueikuei@users.noreply.github.com>
2020-08-05 14:27:26 +0800

    Auto-commit: Wed Aug 05 2020 14:27:09 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
@@ -1,6 +1,6 @@
 with t as(
     select t.slipNo, b.*
-    from momo-develop.ipacking.ipack_temp as t, unnest(t.info) as b
+    from momo-develop.ipacking.ipack_temp_new as t, unnest(t.info) as b
 )
 
 select orderNo,slipNO 
