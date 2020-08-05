0b58ec09d4125888004ea7e633a102e2b6666c02
kueikuei <kueikuei@users.noreply.github.com>
2020-08-05 13:13:53 +0800

    Auto-commit: Wed Aug 05 2020 13:13:36 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -3,6 +3,6 @@ WITH t as(
     from `momo-develop.ipacking.ipack_temp_new`  as t, unnest(t.info) as b
 )
 
-select orderNo, array_agg(slipNO, goodsCode, delyQty, WIDTH, LENGTH, HEIGHT, minProjectArea, lagestSide, boxName)
+select orderNo, array_agg(struct(slipNO, goodsCode, delyQty, WIDTH, LENGTH, HEIGHT, minProjectArea, lagestSide, boxName))
 from t
 group by orderNo
