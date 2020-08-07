97f5533eb9b2eda15d9bff43b032df82c1894906
kueikuei <kueikuei@users.noreply.github.com>
2020-08-07 15:00:46 +0800

    Auto-commit: Fri Aug 07 2020 15:00:29 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -2,7 +2,7 @@
 with t1 as(
 select t.slipNo,b.*
 from `momo-develop.ipacking.ipack_temp_new` as t, UNNEST(info) as b
-)
+),
 t2 as(
 select slipNo,boxName
 from t1
@@ -10,6 +10,6 @@ where boxName is not null
 group by slipNo,boxName
 )
 
-select boxName, count(boxName)/(select count(boxName) from t1)*100 as rate
+select boxName, count(boxName)/(select count(boxName) from t1)*100 as rate, count(boxName) as num
 from t2
 group by boxName
