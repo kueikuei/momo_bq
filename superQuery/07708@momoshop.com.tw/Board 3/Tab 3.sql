71e3dd6bac3b6774fbe6a79e772bad57a43cb5d9
kueikuei <kueikuei@users.noreply.github.com>
2020-08-07 09:59:03 +0800

    Auto-commit: Fri Aug 07 2020 09:58:57 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,15 +1,2 @@
-WITH t1 as(
-  select t.orderNo, b.*
-  from `momo-develop.ipacking.ipack_temp_byOrderNo` as t, unnest(info) as b  
-  -- where slipNo = '10000265366378'
-)
-
-
--- select * from t1
--- where boxName is null
-
-
-select boxName, count(boxName) as num
-from t1
-group by boxName
-order by num desc
+select * from `momo-develop.ipacking.ipack_temp_byOrderNo` as t
+where boxName is not null
