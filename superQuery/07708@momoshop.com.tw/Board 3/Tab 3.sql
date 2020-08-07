10435ad968585c87c61210ba986029a7859ef633
kueikuei <kueikuei@users.noreply.github.com>
2020-08-07 14:25:25 +0800

    Auto-commit: Fri Aug 07 2020 14:25:17 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,3 +1,9 @@
-
-select *
-from `momo-develop.ipacking.ipack_temp_new` as t limit 100
+with t1 as(
+select t.slipNo,b.*
+from `momo-develop.ipacking.ipack_temp_new` as t, UNNEST(info)
+)
+select boxName, count(boxName)/(select count(boxName) from t1 where boxName is not null)*100 as rate
+from t1
+where boxName is not null
+group by boxName
+order by rate desc
