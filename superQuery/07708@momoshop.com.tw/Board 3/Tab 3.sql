b0eb38e6856984eb70ef7cd586a7b99edd96917b
kueikuei <kueikuei@users.noreply.github.com>
2020-08-07 14:52:28 +0800

    Auto-commit: Fri Aug 07 2020 14:52:13 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -2,6 +2,7 @@ with t1 as(
 select t.slipNo,b.*
 from `momo-develop.ipacking.ipack_temp_new` as t, UNNEST(info) as b
 )
-select slipNo, array_agg(struct(distinct boxName))
+select slipNo, array_agg(struct(boxName))
 from t1
 group by slipNo
+-- group by boxNam
