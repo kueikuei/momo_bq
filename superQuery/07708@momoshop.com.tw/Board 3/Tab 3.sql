568a97bf641a0725da908daa0f5b5c806225f92e
kueikuei <kueikuei@users.noreply.github.com>
2020-08-07 09:59:47 +0800

    Auto-commit: Fri Aug 07 2020 09:59:31 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,2 +1,8 @@
-select * from `momo-develop.ipacking.ipack_temp_byOrderNo` as t
+WITH t1 as(
+  select t.orderNo, b.*
+  from `momo-develop.ipacking.ipack_temp_byOrderNo` as t, unnest(info) as b  
+  -- where slipNo = '10000265366378'
+)
+
+select * from t1
 where boxName is not null
