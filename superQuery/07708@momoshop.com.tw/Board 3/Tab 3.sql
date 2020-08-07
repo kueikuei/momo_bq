dd0f21c988b07fd06789a82c396ee1b22588b567
kueikuei <kueikuei@users.noreply.github.com>
2020-08-07 14:58:41 +0800

    Auto-commit: Fri Aug 07 2020 14:58:26 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -1,10 +1,7 @@
-
 with t1 as(
 select t.slipNo,b.*
 from `momo-develop.ipacking.ipack_temp_new` as t, UNNEST(info) as b
 )
-select slipNo as sn, (select * from t1 where slipNo = sn group by boxName)
+select slipNo,boxName
 from t1
-group by slipNo
--- group by boxName
--- order by rate desc
+group by slipNo,boxName
