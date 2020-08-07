d0d6fa00956a329cf59293d73f9df203df06e453
kueikuei <kueikuei@users.noreply.github.com>
2020-08-07 10:03:27 +0800

    Auto-commit: Fri Aug 07 2020 10:03:19 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 3.sql
@@ -4,5 +4,19 @@ WITH t1 as(
   -- where slipNo = '10000265366378'
 )
 
-select * from t1
+
+select boxName, count(boxName)/(select count(boxName) from t1)*100 as rate
+from t1
 where boxName is not null
+group by boxName
+order by num desc
+
+
+-- WITH t1 as(
+--   select t.orderNo, b.*
+--   from `momo-develop.ipacking.ipack_temp_byOrderNo` as t, unnest(info) as b  
+--   -- where slipNo = '10000265366378'
+-- )
+
+-- select * from t1
+-- where boxName is not null
