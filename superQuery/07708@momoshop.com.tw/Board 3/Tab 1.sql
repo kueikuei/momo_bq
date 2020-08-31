94cc705f13debe43f0125a082e2d549a1c0c91b5
kueikuei <kueikuei@users.noreply.github.com>
2020-08-31 17:29:30 +0800

    Auto-commit: Mon Aug 31 2020 17:29:28 GMT+0800 (Taipei Standard Time)

--- superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
+++ superQuery/07708@momoshop.com.tw/Board 3/Tab 1.sql
@@ -1,3 +1,3 @@
-select * 
-from `momo-develop.ipacking.ipack_temp_byOrderNo`
-where orderNo = '20200217625793'
+  SELECT  distinct t.xwhat ,t.xwhen ,t.xwho,t.xcontext.item_id 
+  FROM  `momo-logistics-pro.momodataset.CUSTOMER_LOG_PROCESSED_1d`  as t
+  where xwhat='view_item' and t.xcontext.item_id is not null
